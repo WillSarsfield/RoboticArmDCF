b5c36696ed2e488965cb9f93099659a1aff4de12
q44958jp <joseph.percival-2@student.manchester.ac.uk>
2022-07-21 16:13:22 +0100

    setting up basic UI with custom theme

---
+++ tkinter test.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.tk.call('lappend', 'auto_path', './awthemes-10.4.0')
+root.tk.call('package', 'require', 'awdark')
+style = ttk.Style()
+style.theme_use('awdark')
+l = Listbox(root, height=5)
+l.grid(column=0, row=0, sticky=(N,W,E,S))
+s = ttk.Scrollbar(root, orient=VERTICAL, command=l.yview)
+s.grid(column=1, row=0, sticky=(N,S))
+l['yscrollcommand'] = s.set
+button1 = ttk.Button(root, text="Button1").grid(column=0, columnspan=2, row=1, sticky=(W))
+button3 = ttk.Button(root, text="Button3").grid(column=0, columnspan=2, row=1, sticky=(E))
+button2 = ttk.Button(root, text="Button2").grid(column=0, columnspan=2, row=1)
+root.grid_columnconfigure(0, weight=1)
+root.grid_rowconfigure(0, weight=1)
+for i in range(1,101):
+    l.insert('end', 'Line %d of 100' % i)
+root.mainloop()
