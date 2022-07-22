89fb5eae34e79d7147d17e96fa81c9f00257a0d8
q44958jp <joseph.percival-2@student.manchester.ac.uk>
2022-07-22 14:07:30 +0100

    updating arduino code

--- python_comm/python_comm.ino
+++ python_comm/python_comm.ino
@@ -1,11 +1,32 @@
-int x;
+int x=0;
+int led1=10;
+int led2=11;
+int led3=9;
+int led4=6;
 
 void setup() {
-Serial.begin(9600);
-Serial.setTimeout(1);
+  Serial.begin(9600);
+  Serial.setTimeout(1);
+  pinMode(led1,OUTPUT);
+  pinMode(led2,OUTPUT);
+  pinMode(led3,OUTPUT);
+  pinMode(led4,OUTPUT);
+  digitalWrite(led1,HIGH);
+  delay(100);
+  digitalWrite(led1,LOW);
+  delay(100);
+  digitalWrite(led1,HIGH);
+  delay(100);
+  digitalWrite(led1,LOW);
+  delay(100);
 }
 
 void loop() {
-while (!Serial.available());
-x=Serial.readString().toInt();
-Serial.print(x+1);
+  Serial.print(x);
+  while (!Serial.available()){}
+  x=!Serial.readString().toInt();
+  digitalWrite(led1,x);
+  digitalWrite(led2,x);
+  digitalWrite(led3,x);
+  digitalWrite(led4,x);
+}
