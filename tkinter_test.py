09df546039a4291da086d038ba5498a195e35c79
q44958jp <joseph.percival-2@student.manchester.ac.uk>
2022-07-22 15:07:19 +0100

    added servo control

--- tkinter_test.py
+++ tkinter_test.py
@@ -9,11 +9,13 @@ arduinoPort = '/dev/cu.usbmodem1301' #for mac
 arduino = serial.Serial(port=arduinoPort,baudrate=9600, timeout=.1)
 time.sleep(2)
 
+
+custom_style = 'awdark'
 root = Tk()
 root.tk.call('lappend', 'auto_path', './awthemes-10.4.0')
-root.tk.call('package', 'require', 'awdark')
+root.tk.call('package', 'require', custom_style)
 style = ttk.Style()
-style.theme_use('awdark')
+style.theme_use(custom_style)
 l = Listbox(root, height=5)
 l.grid(column=0, row=0, sticky=(N,W,E,S))
 s = ttk.Scrollbar(root, orient=VERTICAL, command=l.yview)
@@ -22,7 +24,13 @@ l['yscrollcommand'] = s.set
 
 def hello_world():
     os.system('python3 hello_world.py')
+
+def servo_right():
+    arduino.write(bytes("180",'utf-8'))
     
+def servo_left():
+    arduino.write(bytes("0",'utf-8'))
+
 lightState="0"
 def toggle_lights():
     global lightState
@@ -32,9 +40,9 @@ def toggle_lights():
         
 
 
-button1 = ttk.Button(root, text="Hello World", command=hello_world).grid(column=0, columnspan=2, row=1, sticky=(W))
+button1 = ttk.Button(root, text="Move Left", command=servo_left).grid(column=0, columnspan=2, row=1, sticky=(W))
 button3 = ttk.Button(root, text="Toggle Lights", command=toggle_lights).grid(column=0, columnspan=2, row=1, sticky=(E))
-button2 = ttk.Button(root, text="Button2").grid(column=0, columnspan=2, row=1)
+button2 = ttk.Button(root, text="Move Right", command=servo_right).grid(column=0, columnspan=2, row=1)
 root.grid_columnconfigure(0, weight=1)
 root.grid_rowconfigure(0, weight=1)
 for i in range(1,101):
