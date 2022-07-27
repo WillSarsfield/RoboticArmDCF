73dfda30f88c7156fb8e15a5b59c6f42e183b0b2
q44958jp <joseph.percival-2@student.manchester.ac.uk>
2022-07-27 12:02:05 +0100

    added minimum wait time

--- execute_code.py
+++ execute_code.py
@@ -7,7 +7,7 @@ class execute_code():
     def __init__(self):
         self.timeout=.1005
 
-        #arduinoPort = '/dev/cu.usbmodem1101' #for mac - check bottom of arduino editor and modify 
+        # arduinoPort = '/dev/cu.usbmodem1101' #for mac - check bottom of arduino editor and modify 
         arduinoPort = 'COM5' #for windows - may be a different number
 
         self.arduino = serial.Serial(port=arduinoPort,baudrate=115200, timeout=self.timeout)
@@ -17,7 +17,7 @@ class execute_code():
             print(cmd)
             if int(cmd)<0:
                 self.arduino.write(bytes('0','utf-8'))
-                time.sleep(-(int(cmd)+1)/1000)
+                time.sleep(-(int(cmd)+1+min_delay)/1000)
             else:
                 self.arduino.write(bytes(str(cmd),'utf-8'))
             time.sleep(self.timeout)
